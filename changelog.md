6b45b28b626027e7b251a234fd44a15ff8a1d11d
randomtwdude <105645765+randomtwdude@users.noreply.github.com>
2023-12-01 16:28:21 +0800

    v0.4: word-wrapping options and two more fumos

--- changelog.md
+++ changelog.md
@@ -1,10 +1,22 @@
+### v0.4 - 2023/12/01
+- Fumos will now respect *all* your line breaks. (slightly different from cowsay)
+- Added" `-n`  
+  cowsay parity: disables word-wrapping altogether, allowing arbitrary whitespaces  
+  Overwrites `-W`.
+- New fumofumos!  
+  Marisa Kirisame and Flandre Scarlet have agreed to come to your terminal!
+- Art tweaks  
+  Reimu and Patchouli received slight tweaks.
+- Technical changes  
+  Fumos are now stored in a separate file. Not one file for each fumo though, that's kind of hard.
+
 ### v0.3 - 2023/10/27
-- Now fumos can read from standard input!
+- Now fumos can read from standard input!  
   Fumos can now read from files and other programs.
   Newlines in the input are simply ignored for now.
 
 ### v0.2 - 2023/10/20
-- Added: `-W`
+- Added: `-W`  
   cowsay parity: specifies where the message should be wrapped
 - Fixed issues with really narrow text boxes.
 
--- fumosay.c
+++ fumosay.c
@@ -3,24 +3,27 @@
 // fairly barebones for now, more updates to come.
 
 /* ===== INCLUDES ===== */
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdbool.h>
 
 /* ===== DEFINES ===== */
-#define FUMO_COUNT 2
+#define FUMO_COUNT 4
 int MAX_WIDTH = 80;
-enum fumo_who {Reimu, Patchy};
+enum fumo_who {Reimu, Patchy, Marisa, Flandre};
 
 /* ===== FUNCTIONS ===== */
 #define MIN(a,b) (a > b ? b : a)
 #define MAX(a,b) (a > b ? a : b)
 
 void helpInfo() {
-  printf("=== fumosay ver. 0.3 ===\n"
-         "Usage: fumosay [-h] [-W column] *message*\n"
+  printf("=== fumosay ver. 0.4 ===\n"
+         "Usage: fumosay [-hn] [-W column] *message*\n"
+         "-n     Disables word-wrapping.\n"
          "-W     Specifies roughly where the message should be wrapped.\n"
          "       The default is 80.\n"
          "In spirit of the original cowsay, made by Tony Monroe in 1999.\n"
@@ -48,97 +51,54 @@ char *getInput(FILE *st, size_t size) {
   }
   while (1) {
     ch = fgetc(st);
-    if (ch == EOF || ch == '\n' || ch == ' ') {
-      if (ch != EOF && len == 0) {
-        continue; // ignore
+    if (ch == ' ' || ch == 9 /* TAB */) {
+      if (len) {
+        break;
       } else {
-        break; // end token
+        continue;
       }
-    }
+    } else if (ch == '\n') {
+      str[len++] = ch;
+      if (len == size) {
+        str = realloc(str, sizeof(*str) * (size += 16));
+        if (!str) {
+          return str;
+        }
+      } // extend
+      break;
+    } else if (ch == EOF) {
+      break;
+    } // if
+
+    // append new character
     str[len++] = ch;
+    // extend the string if necessary
     if (len == size) {
       str = realloc(str, sizeof(*str) * (size += 16));
       if (!str) {
         return str;
       }
     }
-  }
+  } // while
   str[len++] = '\0';
   return realloc(str, sizeof(*str) * len);
 }
 
-// todo: move to own file
+#include "fumo.fumo"
+// fumo!
 void fumofumo(enum fumo_who fm) {
   switch (fm) {
   case Reimu:
-    fputs(R"(
-       \
-        \                                                *******
-         \         *@@@@*                           .*@@*       @=@@
-          \    #@@@      @@@@@@+.                @@@              @: %=
-           \  @  @                  =@@-     @@.                 *@   @.
-              @  @*                      @@@                      @@   @
-              @   @                  :@@@@@%%%@@@@+               .@   @
-              @  @              =@@.                 %@+         .%    @
-              @  =@+         @@                          @@        @  .*
-              -+   @       @.                              .@     @.  @
-               @. @      %-                                  .@.@@  -@
-                @. @% %@@             %         #                  %@
-                 #@.@:  @    @       #%@        -        .   @   @   .@
-                 =@     @ @  #.   _@#   @-     @.@@=_     @  @@  @     .@
-               .@       .@ @  @@==        %@#=@      ###@@- @  +@.       -@
-              @=        @    @@:==========      ==========+@    @          @
-             @.         *    %.  @  *    @      @  *    @  @    @           @
-            *-          @     #  @*#     @      @**     @  @    @         ##-@
-            @.          %     @  @.     @       *@      @  @    @.  ##@---   @
-           #: -@##_   _.@     @   @=__=@          @=__=@   @   = @--      %@@
-            *@_   *@#-  @     @                            @    @  @  @@@+
-               @@_     @.     @                            @    @   **
-                  +@##-  *@@%%*            ####             @:  /
-                         @    @@_                        _@.@+@@
-                          *@@@#  #@@.                 @@#   @  -*
-                          /   @     @.*@@@=    =@@@* @      @   #
-                          @   @    #*      @- @.     :@     @   @
-                          @,  #      \###@@@   @:@==/       *@*
-                            *^             @. *.
-                                          =    @.
-                                         @      @
-                                        #@@-__-@@#)", stdout);
+    fputs(reimu, stdout);
     break;
   case Patchy:
-    fputs(R"(
-       \
-        \                                  +--*##=_
-         \                       ___         *+    #@
-          \                 +@#@@  #*_==-_##  =     #@#=*
-           \          __ .@*                   %    .%   @.
-            \        @  @:                    @      @    :#
-             \      @   #               @=__+*-     .@     @+=#@
-                   #    @                +_        _@      #   *#
-                 __*                       *.=___=+             @_=**_
-              @@-  -*=_*                                    @  @ *    *#
-           .@     @* %  #==    ==@#@,    @#@    .@@+.   _*@@ == *#@     @
-          @:     @  @      *##*      ---#   #==-     *#*        @  @     #
-           =@  .@   #  *       .      .       .      :.     %   .#  @  @=
-             @=@    @  #       *      #       #      :-      @  @   *#@
-                     # .      =:      @       @       +      @ =
-                       *-----*  *----* +-----+ *----* +----**
-                      #@    @ ==========     ========== *    *
-                      @     @  @ #    %       @ %    %  @    #.
-                      @     @  @+     @       #*     @  @    #.
-                      *.   .*  @      #       @      @  @    @
-                       *   *    #=__=#         #=__=#   *.  @
-                  #= %@.@@ _*=*.                    _*==_ @@ ##==
-                 *     @  @    @          n        #     @  @    *#
-                 *.    _@@.    *                   @.   # @@ +    @
-                  #@__*#  #*=_# * #=,           ,=#_ =-+ #  # #--*
-                       #  # ___      #==,___,==#     _   #  #
-                  *%%=* @@ *   #                  .:# #** @@ **#%#=
-                 @     @  @     @ #=_   @ * @   _= @     @  @      @
-                 *   ,+ @@ *__-+     **=@ * @=**   @  .,# @@ +_   @
-                  +*#  +  +             *   *       **=  +  +  =**
-                       @  @             @-_-@            @  @
-                        ##                                ##)", stdout);
+    fputs(patchouli, stdout);
+    break;
+  case Marisa:
+    fputs(marisa, stdout);
+    break;
+  case Flandre:
+    fputs(flandre, stdout);
     break;
   }
 }
@@ -147,8 +107,16 @@ void fumofumo(enum fumo_who fm) {
 size_t longestLineWidth(int argc, char **argv) {
   size_t cur_line = 0;
   size_t max_line = 0;
+  bool line_break;
+
   for (int i = 0; i < argc; i++) {
+    line_break = false;
     size_t word_len = strlen(argv[i]) + 1;
+    if (argv[i][word_len - 2] == '\n') {
+      word_len -= 1;
+      line_break = true;
+    }
+
     // if word is longer than max, return max
     if (word_len > MAX_WIDTH) {
       return MAX_WIDTH;
@@ -157,10 +125,18 @@ size_t longestLineWidth(int argc, char **argv) {
       // add this word
       cur_line += word_len;
       max_line = MAX(cur_line, max_line);
+      if (line_break) {
+        cur_line = 0;
+      }
     } else {
       // count a new line
       max_line = MAX(cur_line, max_line);
-      cur_line = word_len;
+      if (line_break) {
+        max_line = MAX(word_len, max_line);
+        cur_line = 0;
+      } else {
+        cur_line = word_len;
+      }
     }
   }
   return max_line;
@@ -175,9 +151,18 @@ void paddedBreak(int padding) {
 
 void printMessage(int argc, char **argv, size_t bubble_size) {
   size_t cur_line = 0;
+  bool line_break = false;
+
   for (int i = 0; i < argc; i++) {
 
+
+    line_break = false;
     size_t word_len = strlen(argv[i]) + 1;
+    if (argv[i][word_len - 2] == '\n') {
+      argv[i][word_len - 2] = 0; // strip newline
+      word_len -= 1;
+      line_break = true;
+    }
 
     // left edge
     if (cur_line == 0) {
@@ -187,17 +172,22 @@ void printMessage(int argc, char **argv, size_t bubble_size) {
     // word fits
     if (cur_line + word_len <= bubble_size) {
       printf("%s ", argv[i]);
-      // terminate line if at max length
       if (cur_line + word_len == bubble_size) {
+        // terminate this line if max length
         paddedBreak(0);
+        cur_line = 0;
+        continue;
+      } else if (i == argc - 1) {
+        // terminate all if last word
+        paddedBreak(bubble_size - cur_line - word_len);
+        break;
+      } else if (line_break) {
+        // terminate by newline
+        paddedBreak(bubble_size - cur_line - word_len);
         cur_line = 0;
         continue;
       }
       cur_line += word_len;
-      // last word
-      if (i == argc - 1) {
-        paddedBreak(bubble_size - cur_line);
-      }
       continue;
     }
     // word doesn't fit this line
@@ -214,36 +204,59 @@ void printMessage(int argc, char **argv, size_t bubble_size) {
       for (int j = 0;; j++) {
         size_t len = MIN(bubble_size - 1 /* leave a blank for looks */, strlen(word));
         printf("%.*s", (int)len, word);
-        paddedBreak(bubble_size - len);
         word += len;
         out += len;
+        cur_line = len;
         if (out >= word_len - 1) {
+          if (line_break) {
+            paddedBreak(bubble_size - cur_line);
+            cur_line = 0;
+          } else {
+            printf(" ");
+            cur_line += 1;
+          }
           break;
+        } else {
+          paddedBreak(1);
         }
         printf("( ");
-      }
-      cur_line = 0;
-    } else {
-      printf("%s ", argv[i]);
-      cur_line = word_len;
-      if (cur_line == bubble_size || i == argc - 1) {
-        paddedBreak(bubble_size - cur_line);
+      } // for
+      if (line_break) {
         cur_line = 0;
       }
-    }
-  }
+    } else {
+      // word fits in a single line
+      printf("%s ", argv[i]);
+      cur_line = word_len;
+      if (i == argc - 1) {
+        paddedBreak(bubble_size - cur_line);
+        break;
+      }
+      if (line_break) {
+        paddedBreak(bubble_size - cur_line);
+        printf("( ");
+        paddedBreak(bubble_size);
+        cur_line = 0;
+      }
+    } // word doesn't fit this line
+  } // for each word
 }
 
 int main(int argc, char **argv) {
   // random init
   srand(time(NULL));
+  bool no_wrap = false;
   // argument
   char opt;
-  while ((opt = getopt(argc, argv, "hW:")) != -1) {
+  while ((opt = getopt(argc, argv, "hnW:")) != -1) {
     switch (opt) {
     case 'h':
       helpInfo();
       exit(EXIT_SUCCESS);
+    case 'n':
+      MAX_WIDTH = 100000000;
+      no_wrap = true;
+      break;
     case 'W':
       MAX_WIDTH = numberize(optarg);
       if (MAX_WIDTH < 2) {
@@ -257,16 +270,27 @@ int main(int argc, char **argv) {
   char **word_vector = realloc(NULL, sizeof(char *));
   if (optind == argc) {
     // read from stdin instead
-    while (1) {
-      char *token;
-      token = getInput(stdin, 10);
-      if (strlen(token) == 0) {
-        break;
+    if (no_wrap) {
+      char *token = NULL;
+      size_t line;
+      while (getline(&token, &line, stdin) > -1) {
+        word_vector[word_count++] = token;
+        word_vector = realloc(word_vector, (word_count + 1) * sizeof(char *));
+        token = malloc(120 /* some reasonable numbers */);
       }
-      word_vector[word_count++] = token;
-      word_vector = realloc(word_vector, (word_count + 1) * sizeof(char *));
+      word_vector = realloc(word_vector, word_count * sizeof(char *));
+    } else {
+      while (1) {
+        char *token;
+        token = getInput(stdin, 10);
+        if (strlen(token) == 0) {
+          break;
+        }
+        word_vector[word_count++] = token;
+        word_vector = realloc(word_vector, (word_count + 1) * sizeof(char *));
+      }
+      word_vector = realloc(word_vector, word_count * sizeof(char *));
     }
-    word_vector = realloc(word_vector, word_count * sizeof(char *));
   } else {
     word_count = argc - optind;
     word_vector = argv + optind;
